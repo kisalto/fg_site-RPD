05c6d3008f64cbf973bf3e20ad58fae44260242a
kisalto <joshuabinotto@hotmail.com>
2024-08-08 13:03:27 -0300

    demo project

---
+++ src/main/java/com/example/fgc_site/FgcSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.fgc_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgcSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgcSiteApplication.class, args);
+	}
+
+}
