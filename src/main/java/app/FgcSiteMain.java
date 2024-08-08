fdbfdd742674a774aff2410ee0fac352aef1b98f
kisalto <joshuabinotto@hotmail.com>
2024-08-08 13:30:14 -0300

    Added Event, LastEvent, Game, Character, User and Guide objects

--- src/main/java/app/FgcSiteMain.java
+++ src/main/java/app/FgcSiteMain.java
@@ -1,13 +1,13 @@
-package com.example.fgc_site;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FgcSiteApplication {
+public class FgcSiteMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FgcSiteApplication.class, args);
+		SpringApplication.run(FgcSiteMain.class, args);
 	}
 
 }
