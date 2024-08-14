9655a9440b641b695f7d4a711f8de054bfb37db2
Kauê Cardoso <kauekauada@hotmail.com>
2024-08-13 21:40:33 -0300

    First Att

--- src/main/java/app/entity/Character.java
+++ src/main/java/app/entity/Character.java
@@ -1,5 +1,29 @@
 package app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity 	
+@Getter		
+@Setter		
+@NoArgsConstructor	
+@AllArgsConstructor	
 public class Character {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	// private IMG image;
+	private String Type;
+	
+	//Recebe FK do Jogo
 }
