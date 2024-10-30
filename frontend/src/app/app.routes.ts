91c05fbd3e555140713aab9a3a7d94072373c738
kisalto <joshuabinotto@hotmail.com>
2024-10-29 21:28:26 -0300

    test commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { FighterComponent } from './components/layout/fighter/fighter.component';
+import { FighterListComponent } from './components/fighter/fighter-list/fighter-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'fighter', pathMatch: 'full' },
-    { path: 'fighter', component: FighterComponent }
+    { path: 'fighter', component: FighterComponent },
+    { path: 'fighter-list', component: FighterListComponent },
 ];
--- frontend/src/app/components/fighter/fighter-list/fighter-list.component.ts
+++ frontend/src/app/components/fighter/fighter-list/fighter-list.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './fighter-list.component.scss'
 })
 export class FighterListComponent {
-
+  
 }
