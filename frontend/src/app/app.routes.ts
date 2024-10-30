c3d6fe83155a70ea3eebaa4cfa3709de513235c0
Kauê Cardoso <kauekauada@hotmail.com>
2024-10-29 21:13:45 -0300

    NavBarFeita

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { GamesComponent } from './components/pages/games/games.component';
+import { MainComponent } from './components/layout/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo:"main", pathMatch:"full"},
+    {path: "main", component: MainComponent, children:[
+        {path: "games", component: GamesComponent}
+
+    ]}
+];
--- frontend/src/app/components/layout/main/main.component.ts
+++ frontend/src/app/components/layout/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
