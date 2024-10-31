9e6b578044b14f60171b0f172acdddeab52da293
Joshua Binotto <joshuabinotto2@gmail.com>
2024-10-31 09:18:23 -0300

    added base form component for fighter

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { GamesComponent } from './components/pages/games/games.component';
 import { MainComponent } from './components/layout/main/main.component';
 import { FighterComponent } from './components/layout/fighter/fighter.component';
 import { FighterListComponent } from './components/fighter/fighter-list/fighter-list.component';
+import { FighterFormComponent } from './components/fighter/fighter-form/fighter-form.component';
 
 export const routes: Routes = [
     {path: "", redirectTo:"main", pathMatch:"full"},
@@ -10,6 +11,7 @@ export const routes: Routes = [
         { path: "games", component: GamesComponent },
         { path: 'fighter/:name', component: FighterComponent },
         { path: 'fighter-list', component: FighterListComponent },
+        { path: 'fighter/new', component: FighterFormComponent },
 
     ]}
 ]
---
+++ frontend/src/app/components/fighter/fighter-form/fighter-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fighter-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './fighter-form.component.html',
+  styleUrl: './fighter-form.component.scss'
+})
+export class FighterFormComponent {
+
+}
